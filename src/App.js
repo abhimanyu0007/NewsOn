7e31af8183a2cb6360f44089ce528160a254090a
abhimanyu0007 <babarpatilabhimanyu@gmail.com>
2021-11-12 15:46:09 +0530

    host

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import {
 import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component{
-  page_size =8;
-  //apiKey= process.env.NEWS_API
+  pageSize=8;
+  //apiKey= process.env.REACT_APP_NEWS_API
   state={
     progress:10
   }
 
-  //use arrow func bcouz see above page_size  is like variable, so to use function like variable, we need to use arrow func.
+  //use arrow func bcouz see above pageSize is like variable, so to use function like variable, we need to use arrow func.
   setProgress= (progress)=>{
     this.setState({progress: progress})
   }
@@ -31,31 +31,31 @@ export default class App extends Component{
             height={4}
             //or write above code as: height='4px'
           />
-          {/* <News page_size ={8} country="in" category="general"/> */}
+          {/* <News pageSize={8} country="in" category="general"/> */}
           <Switch>
             <Route exact path="/">
-              <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" page_size ={this.page_size } country="in" category="general"/>
+              <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/>
             </Route>
             <Route exact path="/business">
-              <News setProgress={this.setProgress} apiKey={this.apiKey} key="business" page_size ={this.page_size } country="in" category="business"/>
+              <News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business"/>
             </Route>
             <Route exact path="/entertainment">
-              <News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" page_size ={this.page_size } country="in" category="entertainment"/>
+              <News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>
             </Route>
             <Route exact path="/general">
-              <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" page_size ={this.page_size } country="in" category="general"/>
+              <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/>
             </Route>
             <Route exact path="/health">
-              <News setProgress={this.setProgress} apiKey={this.apiKey} key="health" page_size ={this.page_size } country="in" category="health"/>
+              <News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health"/>
             </Route>
             <Route exact path="/science">
-              <News setProgress={this.setProgress} apiKey={this.apiKey} key="science" page_size ={this.page_size } country="in" category="science"/>
+              <News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science"/>
             </Route>
             <Route exact path="/sports">
-              <News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" page_size ={this.page_size } country="in" category="sports"/>
+              <News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports"/>
             </Route>
             <Route exact path="/technology">
-              <News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" page_size ={this.page_size } country="in" category="technology"/>
+              <News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology"/>
             </Route>
           </Switch>
         </Router>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let{title,description,imageUrl,newsUrl,author,date,source}=this.props;
+        let{title,description,imageUrl,newsUrl,date,source}=this.props;
         return (
             <div className="my-3">
                 <div className="card" style={{background: 'black'}}>
@@ -10,8 +10,9 @@ export class NewsItem extends Component {
                     <div className ="card-body" style={{background: '#2e2a5e'}}>
                     <h5 className ="card-title" style={{color: 'white'}}>{title} <span className="badge rounded-pill bg-warning text-dark">{source}</span></h5>
                     <p className ="card-text" style={{color: 'white'}}>{description}...</p>
-                    <p className="card-text"><small className="text">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+                    <p className="card-text"><small className="text">{/*By {!author?"Unknown":author}*/}Published on : {new Date(date).toGMTString()}</small></p>
                     <a rel="noreferrer" href={newsUrl} target="_blank" className ="btn btn-sm btn-primary">Read More</a>
+                    {/* const url =`https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=a1b7687c45614adc8ee23afa27f3e209` */}
                     </div>
                 </div>
             </div>
